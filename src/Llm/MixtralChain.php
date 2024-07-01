2733dca8920205aca81b93426c7378db53c22c2c
rhaymisonbetini <rhaymison@hotmail.com>
2024-07-01 14:54:59 -0300

    feature/mixtral:mixtral-implementation

--- src/Llm/MixtralChain.php
+++ src/Llm/MixtralChain.php
@@ -2,7 +2,46 @@
 
 namespace Rhaymison\ElephantChain\Llm;
 
-class MixtralChain
-{
+use Partitech\PhpMistral\Messages;
+use Partitech\PhpMistral\MistralClient;
+use Partitech\PhpMistral\MistralClientException;
+use Rhaymison\ElephantChain\Interfaces\ModelChainInterface;
 
+class MixtralChain implements ModelChainInterface
+{
+    private MistralClient $client;
+    private string $modelName;
+    private float $temperature;
+
+    /**
+     * @param string $apiKey
+     * @param string $modelName
+     * @param float $temperature
+     */
+    public function __construct(string $apiKey, string $modelName = 'mistral-large-latest', float $temperature = 0.5)
+    {
+        $this->client = new  MistralClient($apiKey);
+        $this->modelName = $modelName;
+        $this->temperature = $temperature;
+    }
+
+    /**
+     * @param array $prompt
+     * @return string
+     * @throws MistralClientException
+     */
+    public function inference(array $prompt): string
+    {
+        $messages = new Messages();
+        $messages->addSystemMessage($prompt['system']);
+        $messages->addUserMessage($prompt['user']);
+
+        $result = $this->client->chat($messages,
+            [
+                'model' => $this->modelName,
+                'temperature' => $this->temperature,
+            ]
+        );
+        return $result->getMessage();
+    }
 }
